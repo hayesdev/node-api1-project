cc5cdec0424a982f13e049c4ebb6acd5e0165642
Greg Hayes <gthelight@gmail.com>
2020-04-13 20:26:27 -0500

    added server.use(express.json) to parse json request bodies, fixed post/put functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const shortid = require("shortid");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.json({ message: "hello, world!" });
 });
@@ -36,27 +38,42 @@ server.get("/api/users/:id", (req, res) => {
   }
 });
 
-// server.post("/api/users", (req, res) => {
-//   if (!req.body.name) {
-//     return res.status(400).json({
-//       message: "Need a user name.",
-//     });
-//   }
-//   // if (!req.body.id) {
-//   //   return res.status(400).json({
-//   //     message: "Missing a user Id",
-//   //   });
-//   // }
+server.post("/api/users", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      message: "Need a user name.",
+    });
+  }
+  //   // if (!req.body.id) {
+  //   //   return res.status(400).json({
+  //   //     message: "Missing a user Id",
+  //   //   });
+  //   // }
 
-//   const newUser = db.createUser({
-//     // id: shortid,
-//     name: req.body.name,
-//     // bio: "A new user",
-//   });
-//   res.status(201).json(newUser);
-// });
+  const newUser = db.createUser({
+    // id: shortid,
+    name: req.body.name,
+    // bio: "A new user",
+  });
+  res.status(201).json(newUser);
+});
 
-server.put("/api/users/:id", (req, res) => {});
+server.put("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+  if (!user) {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist.",
+    });
+  }
+  // else if (req.body.name || req.body.bio) {
+  // }
+  else {
+    const updatedUser = db.updateUser(user.id, {
+      name: req.body.name || user.name,
+    });
+    res.json(updatedUser);
+  }
+});
 
 server.delete("/api/users/:id", (req, res) => {
   // checking to see if user exists
